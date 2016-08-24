99c552c5c1be21a1b401576bf8b22334866b7d97
abe-sanchez <abraham5001@aol.com>
2016-08-24 15:54:45 -0400

    Update String-1.java

--- String-1.java
+++ String-1.java
@@ -2,3 +2,9 @@
 public String helloName(String name) {
 	return "Hello " + name + "!";
 }
+
+
+//Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+public String makeAbba(String a, String b) {
+  return a + b + b + a;
+}
