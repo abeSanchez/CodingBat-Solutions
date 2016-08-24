a4bd818317a0ee6de7b822d8c37ef55d563430c1
abe-sanchez <abraham5001@aol.com>
2016-08-24 16:06:49 -0400

    Update String-1.java

--- String-1.java
+++ String-1.java
@@ -8,3 +8,10 @@ public String helloName(String name) {
 public String makeAbba(String a, String b) {
   return a + b + b + a;
 }
+
+
+//The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i>
+//which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+public String makeTags(String tag, String word) {
+  return "<" + tag + ">" + word + "</" + tag + ">";
+}
