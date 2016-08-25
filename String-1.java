6b8044ca663fba976921f6ecd331cb3180052bef
abe-sanchez <abraham5001@aol.com>
2016-08-24 21:38:59 -0400

    Update String-1.java

--- String-1.java
+++ String-1.java
@@ -17,7 +17,6 @@ public String makeTags(String tag, String word) {
 }
 
 
-
 //Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g.
 //"<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
 public String makeOutWord(String out, String word) {
