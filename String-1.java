bfd62ae7dd3479752ee9dc4211fc2b0614e6efc1
abe-sanchez <abraham5001@aol.com>
2016-08-24 22:29:16 -0400

    Update String-1.java

--- String-1.java
+++ String-1.java
@@ -24,3 +24,11 @@ public String makeOutWord(String out, String word) {
   String right = out.substring(2, out.length());
   return left + word + right;
 }
+
+
+//Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+public String extraEnd(String str) {
+  int length = str.length();
+  String lastTwo = str.substring(length - 2, length);
+  return lastTwo + lastTwo + lastTwo;
+}
