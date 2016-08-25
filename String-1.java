94192b45b44f14eb790b846f9ac5bf5354cb5438
abe-sanchez <abraham5001@aol.com>
2016-08-24 21:10:39 -0400

    Update String-1.java

--- String-1.java
+++ String-1.java
@@ -15,3 +15,13 @@ public String makeAbba(String a, String b) {
 public String makeTags(String tag, String word) {
   return "<" + tag + ">" + word + "</" + tag + ">";
 }
+
+
+
+//Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g.
+//"<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+public String makeOutWord(String out, String word) {
+  String left = out.substring(0, 2 );
+  String right = out.substring(2, out.length());
+  return left + word + right;
+}
